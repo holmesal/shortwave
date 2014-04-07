c46be36b48156f328a5f20ef73276820e49f59de
Ethan Sherr <dogsforarms@gmail.com>
2014-04-06 18:08:07 -0700

    added png and a uiimage extension for resizing, useful for CALayer.contents = (id)UIImage.CGImage OK?

---
+++ Firechat/UIImage+Resize.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+Resize.h
+//  Earshot
+//
+//  Created by Ethan Sherr on 4/5/14.
+//  Copyright (c) 2014 Buildco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIImage(Resize)
+
+- (UIImage *)scaleToSize:(CGSize)size ;
+
+@end
---
+++ Firechat/UIImage+Resize.m
@@ -0,0 +1,38 @@
+//
+//  UIImage+Resize.m
+//  Earshot
+//
+//  Created by Ethan Sherr on 4/5/14.
+//  Copyright (c) 2014 Buildco. All rights reserved.
+//
+
+#import "UIImage+Resize.h"
+
+@implementation UIImage(Resize)
+
+- (UIImage *)scaleToSize:(CGSize)size {
+    // Scalling selected image to targeted size
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(NULL, size.width, size.height, 8, 0, colorSpace, kCGImageAlphaPremultipliedLast);
+    CGContextClearRect(context, CGRectMake(0, 0, size.width, size.height));
+    
+    if (self.imageOrientation == UIImageOrientationRight) {
+        CGContextRotateCTM(context, -M_PI_2);
+        CGContextTranslateCTM(context, -size.height, 0.0f);
+        CGContextDrawImage(context, CGRectMake(0, 0, size.height, size.width), self.CGImage);
+    } else {
+        CGContextDrawImage(context, CGRectMake(0, 0, size.width, size.height), self.CGImage);
+    }
+    
+    CGImageRef scaledImage=CGBitmapContextCreateImage(context);
+    
+    CGColorSpaceRelease(colorSpace);
+    CGContextRelease(context);
+    
+    UIImage * image = [UIImage imageWithCGImage: scaledImage];
+    CGImageRelease(scaledImage);
+    
+    return image;
+}
+
+@end
