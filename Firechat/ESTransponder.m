732dc80ba4f5be04e73d2fb2a7db4b345460c118
holmesal <siralonsoholmes@gmail.com>
2014-04-10 11:17:21 -0700

    minor changes

--- Firechat/ESTransponder.m
+++ Firechat/ESTransponder.m
@@ -23,7 +23,7 @@
 
 #define MAX_BEACON 19 // How many beacons to use (IOS max 19)
 #define TIMEOUT 30.0 // How old should a user be before I consider them gone?
-#define REPORTING_INTERVAL 10.0 // How often to report to firebase
+#define REPORTING_INTERVAL 12.0 // How often to report to firebase
 #define BEACON_TIMEOUT 10.0 // How long to range when a beacon is discovered (background only)
 
 @interface ESTransponder() <CBPeripheralManagerDelegate, CBCentralManagerDelegate, CLLocationManagerDelegate>
