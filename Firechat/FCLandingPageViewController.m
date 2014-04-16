01e4fc754727cef4caeea949929b5352f60f55da
Ethan Sherr <dogsforarms@gmail.com>
2014-04-16 14:13:10 -0700

    OMG, && NO

--- Firechat/FCLandingPageViewController.m
+++ Firechat/FCLandingPageViewController.m
@@ -664,7 +664,7 @@ typedef enum
     FCUser *owner = [FCUser owner];
 
 //#warning REMOVE THIS BEFORE DEPLOY
-    if (owner.beacon.stackIsRunning != ESTransponderStackStateActive && NO)
+    if (owner.beacon.stackIsRunning != ESTransponderStackStateActive)// && NO)
     {
         [owner.beacon startBroadcasting];
         [owner.beacon startDetecting];
