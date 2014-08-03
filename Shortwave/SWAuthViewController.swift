ce6fd31e432a92b80a75a075bef15908828f55d5
Ethan Sherr <dogsforarms@gmail.com>
2014-08-03 16:04:10 -0700

    closes #133

--- Shortwave/SWAuthViewController.swift
+++ Shortwave/SWAuthViewController.swift
@@ -61,7 +61,7 @@ class SWAuthViewController: UIViewController, UIAlertViewDelegate
                     println("error: \(error)");
                     let isMain = NSThread.isMainThread()
                     println("isMainthread? \(isMain)")
-                    var alert: UIAlertView = UIAlertView(title:"No account found.", message: "I was unable to find a Facebook account on this device, are you sure your phone has a Facebook account?  Go to settings then privacy.", delegate: self, cancelButtonTitle: nil, otherButtonTitles:"I'll check")
+                    var alert: UIAlertView = UIAlertView(title:"No account found.", message: "I was unable to find a Facebook account on this device, are you sure your phone has a Facebook account?  Go to settings > facebook.", delegate: self, cancelButtonTitle: nil, otherButtonTitles:"I'll check")
                     alert.show()
                     
                 } else
--- Shortwave/SWImageCell.m
+++ Shortwave/SWImageCell.m
@@ -1,4 +1,5 @@
 
+
 //
 //  SWImageCell.m
 //  Shortwave
